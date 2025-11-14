09653c5abf0764e901f5a57805196ff654b07b5a
kaylas000 <kaylas000@yandex.com>
2025-11-14 23:31:19 +0300

    Добавлен файл с константами для фронтенда — js/utils/constants.js, для единых сообщений и url.

---
+++ js/utils/constants.js
@@ -0,0 +1,5 @@
+// Вспомогательный файл констант
+export const API_URL = '/api';
+export const MSG_CART_ADDED = 'Товар добавлен в корзину';
+export const MSG_LOGIN_FAIL = 'Ошибка входа';
+export const MSG_ORDER_OK = 'Заказ успешно создан';
